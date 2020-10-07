705d2190d6436bac3fffe2bfcb54126b156e7395
lucianoortizsilva <lucianoortizsilva@gmail.com>
2020-10-07 20:14:24 -0300

    Add Configuração Swagger

--- src/main/java/com/lucianoortizsilva/crud/config/ProfileTestConfig.java
+++ src/main/java/com/lucianoortizsilva/crud/config/ProfileTestConfig.java
@@ -9,13 +9,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.google.common.base.Predicates;
 import com.lucianoortizsilva.crud.cliente.model.Cliente;
 import com.lucianoortizsilva.crud.cliente.repository.ClienteRepository;
 
 import lombok.extern.slf4j.Slf4j;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Slf4j
 @Configuration
+@EnableSwagger2
 @Profile(value = "test")
 public class ProfileTestConfig {
 
@@ -36,6 +43,15 @@ public class ProfileTestConfig {
 		final List<Cliente> clientesCadastrados = this.clienteRepository.saveAll(Arrays.asList(clientes));
 		clientesCadastrados.forEach(c -> log.info("Cliente: {}", c));
 
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(Predicates.not(PathSelectors.regex("/error")))
+				.build();
 	}
 
 }
