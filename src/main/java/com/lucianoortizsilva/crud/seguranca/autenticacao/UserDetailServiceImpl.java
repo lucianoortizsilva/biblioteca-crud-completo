34c42fdd53edae6be795ecf091684aef2bad317a
lucianoortizsilva <lucianoortizsilva@gmail.com>
2022-01-14 14:45:03 -0300

    Encapsulando dados do usuário

--- src/main/java/com/lucianoortizsilva/crud/seguranca/autenticacao/UserDetailServiceImpl.java
+++ src/main/java/com/lucianoortizsilva/crud/seguranca/autenticacao/UserDetailServiceImpl.java
@@ -3,6 +3,7 @@ package com.lucianoortizsilva.crud.seguranca.autenticacao;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,12 +11,17 @@ import org.springframework.stereotype.Service;
 
 import com.lucianoortizsilva.crud.pessoa.model.Pessoa;
 import com.lucianoortizsilva.crud.pessoa.repository.PessoaRepository;
+import com.lucianoortizsilva.crud.seguranca.token.Payload;
+import com.lucianoortizsilva.crud.seguranca.token.TokenJwt;
 
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
+
+	@Autowired
+	private TokenJwt tokenJwt;
 
 	@Override
 	public UserDetails loadUserByUsername(final String email) throws UsernameNotFoundException {
@@ -24,6 +30,14 @@ public class UserDetailServiceImpl implements UserDetailsService {
 			throw new UsernameNotFoundException(email);
 		}
 		return new UserDetailsCustom(pessoa.get().getId(), pessoa.get().getEmail(), pessoa.get().getSenha(), pessoa.get().getPerfis());
+	}
+	
+	
+	
+	public UsernamePasswordAuthenticationToken getUsernamePasswordAuthenticationToken(final String authorization) {
+		final Payload payload = this.tokenJwt.getPayload(authorization);
+		final UserDetails userDetails = this.loadUserByUsername(payload.getLogin());
+		return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 	}
 
 }
--- src/main/java/com/lucianoortizsilva/crud/seguranca/filter/ExceptionHandlerFilter.java
+++ src/main/java/com/lucianoortizsilva/crud/seguranca/filter/ExceptionHandlerFilter.java
@@ -14,17 +14,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.lucianoortizsilva.crud.seguranca.autenticacao.UserDetailServiceImpl;
 import com.lucianoortizsilva.crud.seguranca.error.GeraErroInesperado;
 import com.lucianoortizsilva.crud.seguranca.error.GeraErroNaoAutorizado;
 import com.lucianoortizsilva.crud.seguranca.error.GeraErroRequisicaoInvalida;
-import com.lucianoortizsilva.crud.seguranca.token.Payload;
-import com.lucianoortizsilva.crud.seguranca.token.TokenJwt;
 import com.lucianoortizsilva.crud.seguranca.token.TokenJwtException;
 
 import io.jsonwebtoken.ExpiredJwtException;
@@ -41,10 +38,7 @@ import lombok.extern.slf4j.Slf4j;
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
 
 	@Autowired
-	private TokenJwt tokenJwt;
-
-	@Autowired
-	private UserDetailsService userDetailsService;
+	private UserDetailServiceImpl userDetailsServiceImpl;
 
 	@Override
 	public void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain filterChain) throws ServletException, IOException {
@@ -52,10 +46,10 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
 			filterChain.doFilter(request, response);
 		} catch (final Exception e) {
 			if (e.getCause() instanceof AccessDeniedException) {
-				this.autenticar(request, response, filterChain);
+				autenticar(request, response, filterChain);
 			} else if (e.getCause() instanceof SignatureException) {
-				this.autenticar(request, response, filterChain);
-			} else if(e.getCause() instanceof ExpiredJwtException) {
+				autenticar(request, response, filterChain);
+			} else if (e.getCause() instanceof ExpiredJwtException) {
 				final GeraErroNaoAutorizado geraErroNaoAutorizado = new GeraErroNaoAutorizado(response);
 				geraErroNaoAutorizado.comMensagem("Token Expirado");
 			} else {
@@ -67,41 +61,29 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
 	}
 
 	private void autenticar(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain) {
-		final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
-		
-		if (isEmpty(authorization) || !authorization.startsWith("Bearer")) {
-			negarAcessoRequisicaoInvalida(response);
-		} else {
-			try {
-				final UsernamePasswordAuthenticationToken usuarioAutenticado = getUsernamePasswordAuthenticationToken(authorization);
+		try {
+			final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+			if (isEmpty(authorization) || !authorization.startsWith("Bearer")) {
+				GeraErroRequisicaoInvalida geraErroRequisicaoInvalida = new GeraErroRequisicaoInvalida(response);
+				geraErroRequisicaoInvalida.comMensagem("Authorization inválida");
+			} else {
+				final UsernamePasswordAuthenticationToken usuarioAutenticado = this.userDetailsServiceImpl.getUsernamePasswordAuthenticationToken(authorization);
 				SecurityContextHolder.getContext().setAuthentication(usuarioAutenticado);
 				chain.doFilter(request, response);
-			} catch (final UsernameNotFoundException e) {
-				log.error(e.getMessage(), e);
-				GeraErroNaoAutorizado geraErroNaoAutorizado = new GeraErroNaoAutorizado(response);
-				geraErroNaoAutorizado.comMensagem("Authorization com usuário não encontrado");
-			} catch (final TokenJwtException e) {
-				log.error(e.getMessage(), e);
-				GeraErroNaoAutorizado geraErroNaoAutorizado = new GeraErroNaoAutorizado(response);
-				geraErroNaoAutorizado.comMensagem(e.getMessage());
-			} catch (final Exception e) {
-				log.error(e.getMessage(), e);
-				GeraErroNaoAutorizado geraErroNaoAutorizado = new GeraErroNaoAutorizado(response);
-				geraErroNaoAutorizado.comMensagem("Authorization inválida");
 			}
+		} catch (final UsernameNotFoundException e) {
+			log.error(e.getMessage(), e);
+			GeraErroNaoAutorizado geraErroNaoAutorizado = new GeraErroNaoAutorizado(response);
+			geraErroNaoAutorizado.comMensagem("Authorization com usuário não encontrado");
+		} catch (final TokenJwtException e) {
+			log.error(e.getMessage(), e);
+			GeraErroNaoAutorizado geraErroNaoAutorizado = new GeraErroNaoAutorizado(response);
+			geraErroNaoAutorizado.comMensagem(e.getMessage());
+		} catch (final Exception e) {
+			log.error(e.getMessage(), e);
+			GeraErroNaoAutorizado geraErroNaoAutorizado = new GeraErroNaoAutorizado(response);
+			geraErroNaoAutorizado.comMensagem("Authorization inválida");
 		}
-	}
-
-	private UsernamePasswordAuthenticationToken getUsernamePasswordAuthenticationToken(final String authorization) {
-		final Payload payload = this.tokenJwt.getPayload(authorization);
-		final UserDetails userDetails = this.userDetailsService.loadUserByUsername(payload.getLogin());
-		return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-	}
-
-	private static void negarAcessoRequisicaoInvalida(final HttpServletResponse response) {
-		SecurityContextHolder.clearContext();
-		GeraErroRequisicaoInvalida geraErroRequisicaoInvalida = new GeraErroRequisicaoInvalida(response);
-		geraErroRequisicaoInvalida.comMensagem("Authorization inválida");
 	}
 
 }
