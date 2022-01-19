a363b282cc94c276c3a9f8b0857ba5cc66bea78a
Luciano Ortiz Silva <lucianoortizsilva@gmail.com>
2022-01-19 09:00:47 -0300

    Add test de integração para 'DELETE /clientes/{id}'

---
+++ src/test/java/com/lucianoortizsilva/crud/cliente/annotation/WithMockUserCliente.java
@@ -0,0 +1,10 @@
+package com.lucianoortizsilva.crud.cliente.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import org.springframework.security.test.context.support.WithMockUser;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithMockUser(roles = "CLIENTE")
+public @interface WithMockUserCliente {}
--- src/test/java/com/lucianoortizsilva/crud/cliente/controller/ClienteControllerTest.java
+++ src/test/java/com/lucianoortizsilva/crud/cliente/controller/ClienteControllerTest.java
@@ -1,6 +1,7 @@
 package com.lucianoortizsilva.crud.cliente.controller;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -18,11 +19,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.lucianoortizsilva.crud.cliente.annotation.WithMockUserAdmin;
+import com.lucianoortizsilva.crud.cliente.annotation.WithMockUserCliente;
+import com.lucianoortizsilva.crud.cliente.annotation.WithMockUserSemPermissao;
+import com.lucianoortizsilva.crud.cliente.annotation.WithMockUserSuporte;
 import com.lucianoortizsilva.crud.cliente.entity.Cliente;
 import com.lucianoortizsilva.crud.cliente.repository.ClienteRepository;
 
@@ -46,35 +50,72 @@ class ClienteControllerTest {
 	}
 
 	@Nested
-	@WithMockUser
-	@DisplayName("DADO QUE estou autenticado sem permissões de acesso para /clientes/{id}")
-	class SemPermissao {
+	@DisplayName("#Testando > GET /clientes/{id}")
+	class GetClientes {
 
 		@Test
-		@WithMockUser
-		@DisplayName("QUANDO acesso /clientes/{id}, ENTÃO deverá lançar erro 403")
+		@WithMockUserSemPermissao
+		@DisplayName("DADO QUE estou autenticado sem permissões de acesso QUANDO acesso GET /clientes/{id}, ENTÃO deverá lançar erro 403")
 		void clientesById_deveGerarForbidden() throws Exception {
 			mvc.perform(get("/clientes/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isForbidden());
 		}
 
-	}
-
-	@Nested
-	@WithMockUser(roles = { "ADMIN", "CLIENTE", "SUPORTE" })
-	@DisplayName("DADO QUE estou autenticado com permissões de acesso para /clientes/{id}")
-	class ComPermissão {
-
 		@Test
-		@DisplayName("E cliente pesquisado não existe, QUANDO acesso /clientes/{id}, ENTÃO deverá lançar erro 404")
+		@WithMockUserAdmin
+		@DisplayName("DADO QUE estou autenticado com permissão de acesso, E cliente pesquisado não existe, QUANDO acesso GET /clientes/{id}, ENTÃO deverá lançar erro 404")
 		void clientesById_deveGerarNotFound() throws Exception {
 			mvc.perform(get("/clientes/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isNotFound());
 		}
 
 		@Test
-		@DisplayName("E cliente pesquisado existe, QUANDO acesso /clientes/{id}, ENTÃO deverá retornar 200")
+		@WithMockUserAdmin
+		@DisplayName("DADO QUE estou autenticado com permissão de acesso, E cliente pesquisado existe, QUANDO acesso GET /clientes/{id}, ENTÃO deverá retornar 200")
 		void clientesById_deveRetornarComSucesso() throws Exception {
 			Mockito.when(clienteRepository.findById(1L)).thenReturn(Optional.of(new Cliente()));
 			mvc.perform(get("/clientes/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+		}
+
+	}
+
+	
+	@Nested
+	@DisplayName("#Testando > DELETE /clientes/{id}")
+	class DeleteClientes {
+
+		@Test
+		@WithMockUserSemPermissao
+		@DisplayName("DADO QUE estou autenticado sem permissões de acesso, QUANDO acesso DELETE /clientes/{id}, ENTÃO deverá lançar erro 403")
+		void clientesById_deveGerarForbidden() throws Exception {
+			mvc.perform(delete("/clientes/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isForbidden());
+		}
+
+		@Test
+		@WithMockUserCliente
+		@DisplayName("DADO QUE estou autenticado, E estou com usuário com permissão de cliente, QUANDO acesso DELETE /clientes/{id}, ENTÃO deverá lançar erro 403")
+		void clientesById_comUsuarioCliente_deveGerarForbidden() throws Exception {
+			mvc.perform(delete("/clientes/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isForbidden());
+		}
+		
+		@Test
+		@WithMockUserSuporte
+		@DisplayName("DADO QUE estou autenticado, E estou com usuário com permissão de suporte, QUANDO acesso DELETE /clientes/{id}, ENTÃO deverá lançar erro 403")
+		void clientesById_comUsuarioSuporte_deveGerarForbidden() throws Exception {
+			mvc.perform(delete("/clientes/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isForbidden());
+		}
+		
+		@Test
+		@WithMockUserAdmin
+		@DisplayName("DADO QUE estou autenticado com permissão de acesso, E cliente pesquisado não existe, QUANDO acesso DELETE /clientes/{id}, ENTÃO deverá lançar erro 404")
+		void clientesById_deveGerarNotFound() throws Exception {
+			mvc.perform(delete("/clientes/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isNotFound());
+		}
+
+		@Test
+		@WithMockUserAdmin
+		@DisplayName("DADO QUE estou autenticado com permissão de acesso, E cliente pesquisado existe, QUANDO acesso DELETE /clientes/{id}, ENTÃO deverá retornar 200")
+		void clientesById_deveRetornarComSucesso() throws Exception {
+			Mockito.when(clienteRepository.findById(1L)).thenReturn(Optional.of(new Cliente()));
+			mvc.perform(delete("/clientes/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isNoContent());
 		}
 
 	}
